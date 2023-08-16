150bf4bc8e0cae4ed480ed49a0841fd59db1ca39
mohammed arbaz shah <85033772+ar0097@users.noreply.github.com>
2023-08-16 12:34:24 +0530

    Initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header flex">
+      <h2>Arbaz Shah Directory App</h2>
+    </div>
+  );
+}
+
+export default Header;
